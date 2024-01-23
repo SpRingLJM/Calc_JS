d7bb95f0f996e2a9b276f9f00d6f0175d1fc357b
SpRingLJM <81spring7s@gmail.com>
2024-01-23 17:01:25 +0900

    2024/1/23_1

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -198,8 +198,10 @@ function printLogHander() {
   } */
   let  i = 0;
   for (const logEntry of battleLog){
-    console.log(logEntry);
-    console.log(i);
+    console.log(`#${i}`);
+    for(const key in logEntry){
+      console.log(`${key} => ${logEntry[key]}`);
+    }
     i++;
   }
   console.log(battleLog);
