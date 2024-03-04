8a9ea30d343b496a84f77b43f97c4c7f7a4c2fdc
SpRingLJM <81spring7s@gmail.com>
2024-03-05 00:25:19 +0900

    2024/3/4_2

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -12,7 +12,34 @@ class Product {
     }
 }
 
-class ShoppingCart {
+    class ElementAttribute {
+        constructor(attrName, attrValue){
+            this.name = attrName;
+            this.value = attrValue;
+        }
+    }
+
+    class Component{
+        constructor(renderHookId){
+            this.hookId = renderHookId;
+        }
+        
+        createRootElement(tag, cssClasses, attributes){
+            const rootElement = document.createElement(tag);
+            if(cssClasses) {
+                rootElement.className = cssClasses;
+            }
+            if(attributes && attributes.length > 0) {
+                for(const attr of attributes){
+                    rootElement.setAttribute(attr.name, attr.value);
+                }
+            }
+            document.getElementById(this.hookId).append(rootElement);
+            return rootElement; 
+        }
+    }
+
+class ShoppingCart extends Component {
     items = [];
 
     set cartItems(value){
@@ -32,6 +59,10 @@ class ShoppingCart {
         return sum;
     }
 
+    constructor(renderHookId) {
+        super(renderHookId);
+    }
+
     addProdcut(product) {
         const updatedItems = [...this.items];
         updatedItems.push(product);
@@ -40,19 +71,19 @@ class ShoppingCart {
     }
 
     render() {
-        const cartEl = document.createElement('section');
+        const cartEl = this.createRootElement('section', 'cart');
         cartEl.innerHTML =`
             <h2>Total: \$${0}</h2>
             <button>Order Now!</button>
             `;
-            cartEl.className = 'cart';
             this.totalOutput = cartEl.querySelector('h2');
-            return cartEl;
     }
 }
-class ProductItem {
-    constructor(product) {
+class ProductItem extends Component {
+    constructor(product, renderHookId) {
+        super(renderHookId);
         this.product = product;
+        // In this case, calling "super" should precede tasks related to "this".
     }
 
     addToCart() {
@@ -60,8 +91,7 @@ class ProductItem {
     }
 
     render() {
-        const prodEl = document.createElement('li');
-        prodEl.className = 'product-item';
+        const prodEl = this.createRootElement('li', 'product-item');
         prodEl.innerHTML = `
         <div>
             <img src="${this.product.imageUrl}" alt="${this.product.title}" >
@@ -75,11 +105,10 @@ class ProductItem {
         `;
         const addCartButton = prodEl.querySelector('button');
         addCartButton.addEventListener('click', this.addToCart.bind(this));
-        return prodEl;
     }
 }
 // below the class has a 'products' array.
-class ProductList {
+class ProductList extends Component {
     products = [
         new Product(
             'A Pillow',
@@ -94,33 +123,26 @@ class ProductList {
         )
     ];
 
-    constructor() {
-        
+    constructor(renderHookId) {
+        super(renderHookId);
     }
 
     render() {
-    const prodList = document.createElement('ul'); 
-    // ul = unordered list
-    prodList.className = 'product-list';
-    for(const prod of this.products) {
-        const productItem = new ProductItem(prod);
-        const prodEl = productItem.render(); 
-        prodList.append(prodEl);
-    }
-        return prodList;
+        const prodList = this.createRootElement('ul', 'product-list', [new ElementAttribute('id', 'prod-list')]);
+        // ul = unordered list
+        for(const prod of this.products) {
+            const productItem = new ProductItem(prod, 'prod-list');
+            productItem.render();
+        }
     }
 }
 
 class Shop {
     render(){
-        const renderHook = document.getElementById('app');
-        this.cart = new ShoppingCart();
-        const cartEl = this.cart.render();
-        const productList = new ProductList();
-        const prodListEl = productList.render();
-
-        renderHook.append(cartEl);
-        renderHook.append(prodListEl);
+        this.cart = new ShoppingCart('app');
+        this.cart.render();
+        const productList = new ProductList('app');
+        productList.render();
     }
 }
 
@@ -140,4 +162,4 @@ class App {  // static property, static method -> static init()
 
 App.init();
 
-// 28 Line : if you want to use standard dollar character, you will use 'Escape Character(\$)'
+// 49, 76, 100 Line : if you want to use standard dollar character, you will use 'Escape Character(\$)'
