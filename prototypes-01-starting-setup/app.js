a88a00c21f2d4ad96dd6baac109778c0f7692ba2
SpRingLJM <81spring7s@gmail.com>
2024-03-09 18:17:24 +0900

    2024/3/9

--- prototypes-01-starting-setup/app.js
+++ prototypes-01-starting-setup/app.js
@@ -1,8 +1,15 @@
-class Person { // class
+class AgedPerson {
+    printAge(){
+        console.log(this.age);
+    }
+}
+
+class Person extends AgedPerson { // class
     // fields
     name = 'Max';
 
     constructor() { // constructor function
+        super();
         this.age = 30;
     } 
 
@@ -11,16 +18,37 @@ class Person { // class
     };
 }
 
-function Person () {
-    this.age = 30;
-    this.name = 'Max';
-    this.greet = function(){
-        console.log(
-            'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
-        );
-    };
-}
+// function Person () {
+//     this.age = 30;
+//     this.name = 'Max';
+//     this.greet = function(){
+//         console.log(
+//             'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+//         );
+//     };
+// }
 
-const person = new Person();
-// You can instantiate 'new Person' by calling it.
-person.greet();
+// Person.describe = function() {
+//     console.log('Creating persons...');
+// }
+
+// Person.prototype.printAge = function(){
+//     console.log(this.age);
+// };
+
+// console.dir(Person);
+
+// const person = new Person();
+// // You can instantiate 'new Person' by calling it.
+// person.greet();
+// person.printAge();
+// console.log(person.__proto__);
+// console.log(person.toString());
+// const p2 = new person.__proto__.constructor();
+// console.dir(Object);
+// // __proto__ is in all JS objects.
+// // by the way, 'prototype' does not exist in all objects and exists in function objects.
+// // Object.prototype has not __proto__. So, Object.prototype is where the prototype chain ends.
+
+const p = new Person();
+console.log(p);
